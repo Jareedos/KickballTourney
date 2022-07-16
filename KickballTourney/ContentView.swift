ee11882a42792749b5804332dd950fc1aac2b2e1
Jared Sobol <jaredsobol@jareds-mbp.lan>
2022-07-16 16:21:34 -0700

    Created Project

---
+++ KickballTourney/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KickballTourney
+//
+//  Created by Jared Sobol on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KickballTourney/KickballTourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickballTourneyApp.swift
+//  KickballTourney
+//
+//  Created by Jared Sobol on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct KickballTourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
